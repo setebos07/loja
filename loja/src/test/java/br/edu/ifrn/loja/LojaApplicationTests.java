05ab5e49f94310540bfbe92092f9060827c0d05f
setebos07 <setebos07@users.noreply.github.com>
2020-03-05 14:52:09 -0300

    loja

---
+++ loja/src/test/java/br/edu/ifrn/loja/LojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
